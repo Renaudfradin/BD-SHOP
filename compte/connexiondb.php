7bf2136938a24c959a32252f9f2442c3c1b69121
Renaud <48203798+Renaudfradin@users.noreply.github.com>
2020-08-26 11:39:36 +0200

    Add files via upload

---
+++ compte/connexiondb.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+
+$pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+$bdd = new PDO('mysql:host=localhost;dbname=projet_bandes_dessines;charset=utf8', 'root', '',$pdo_options);
+
+
+?>
